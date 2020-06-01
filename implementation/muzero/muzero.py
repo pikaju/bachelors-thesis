6be0d6c9880b5885bc5a56372e4e3a6053fac2d4
Julien Scholz <julienscholzo@hotmail.de>
2020-06-01 13:03:26 +0200

    Start implementation

---
+++ implementation/muzero/muzero.py
@@ -0,0 +1,5 @@
+class MuZero:
+    def __init__(self, representation, dynamics, prediction):
+        self.representation = representation
+        self.dynamics = dynamics
+        self.prediction = prediction
