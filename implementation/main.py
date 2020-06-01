72e0d22e0254910c98e4a94dcd0c8fa54f4239ca
Julien Scholz <julienscholzo@hotmail.de>
2020-06-01 13:53:46 +0200

    Switch to pipenv

--- implementation/main.py
+++ implementation/main.py
@@ -1,14 +1,11 @@
 import tensorflow as tf
 import gym
-import baselines
 
 from gym import envs
 
 
 def main():
     print(tf.add(tf.constant(2), tf.constant(3)))
-    env = gym.make('CartPole-v0')
-    print(env)
 
 
 if __name__ == '__main__':
