4d7922474118b674edbefb23f0300f8f9c0606c9
Julien Scholz <julienscholzo@hotmail.de>
2020-06-07 16:05:03 +0200

    Add basic environment loop

--- implementation/main.py
+++ implementation/main.py
@@ -8,7 +8,16 @@ from baselines.deepq.replay_buffer import ReplayBuffer
 
 def main():
     env = gym.make('CartPole-v0')
-    env.reset()
+    while True:
+        obs_t = env.reset()
+        while True:
+            env.render()
+            action = env.action_space.sample()
+            obs_tp1, reward, done, info = env.step(action)
+
+            if done:
+                break
+            obs_t = obs_tp0
 
 
 if __name__ == '__main__':
