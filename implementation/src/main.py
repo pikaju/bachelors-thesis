984c28221825fc21e68a1a6c22642ed8be1ec4a4
Julien Scholz <julienscholzo@hotmail.de>
2020-06-14 13:09:32 +0200

    Add spot talk

--- implementation/src/main.py
+++ implementation/src/main.py
@@ -79,6 +79,10 @@ def define_model(env):
     return representation, dynamics, prediction, action_sampler, variables
 
 
+# ADD REWARDS WHEN PLANNING, FUTURE VALUE ALONE IS NOT THE ONLY THING THAT MATTERS
+
+
+
 def define_losses(variables):
     def loss_r(true, pred):
         return tf.losses.MSE(true, pred)
