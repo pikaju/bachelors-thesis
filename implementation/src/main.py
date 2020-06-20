8317f38872d59c66ad33c7b75933ec127e03bc62
Julien Scholz <julienscholzo@hotmail.de>
2020-06-20 22:47:26 +0200

    Add support for continuous actions

--- implementation/src/main.py
+++ implementation/src/main.py
@@ -26,7 +26,10 @@ def define_representation(env):
 def define_model(env):
     representation, representation_variables = define_representation(env)
 
-    action_shape = env.action_space.n
+    if isinstance(env.action_space, gym.spaces.Box):
+        action_shape = env.action_space.shape[0]
+    else:
+        action_shape = env.action_space.n
 
     dynamics_trunk = tf.keras.Sequential([
         tf.keras.layers.Dense(state_shape, activation=tf.nn.leaky_relu,
@@ -45,7 +48,8 @@ def define_model(env):
         [dynamics_trunk, dynamics_state_head], name='dynamics_state')
 
     def dynamics(state, action):
-        action = tf.one_hot(action, action_shape, axis=-1)
+        if isinstance(env.action_space, gym.spaces.Discrete):
+            action = tf.one_hot(action, action_shape, axis=-1)
         state_action = tf.concat((state, action), axis=1)
         return (tf.reshape(dynamics_reward_path(state_action), [-1]), dynamics_state_path(state_action))
 
@@ -69,7 +73,10 @@ def define_model(env):
         return (prediction_policy_path(state), tf.reshape(prediction_value_path(state), [-1]))
 
     def action_sampler(policy):
-        return tf.reshape(tf.random.categorical(policy, num_samples=1), [-1])
+        if isinstance(env.action_space, gym.spaces.Discrete):
+            return tf.reshape(tf.random.categorical(policy, num_samples=1), [-1])
+        else:
+            return tf.random.normal(policy.shape, mean=policy)
 
     variables = [
         *representation_variables,
@@ -82,15 +89,18 @@ def define_model(env):
     return representation, dynamics, prediction, action_sampler, variables
 
 
-def define_losses(variables):
+def define_losses(env, variables):
     def loss_r(true, pred):
         return tf.losses.MSE(true, pred)
 
     def loss_v(true, pred):
         return tf.losses.MSE(true, pred)
 
-    def loss_p(action, logits):
-        return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(action, logits))
+    def loss_p(action, policy):
+        if isinstance(env.action_space, gym.spaces.Discrete):
+            return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(action, policy))
+        else:
+            return tf.losses.MSE(action, policy)
 
     def regularization():
         return tf.add_n([tf.nn.l2_loss(variable) for variable in variables]) * 0.001
@@ -99,19 +109,19 @@ def define_losses(variables):
 
 
 def main():
-    env = gym.make('LunarLander-v2')
+    env = gym.make('LunarLanderContinuous-v2')
     discount_factor = 0.95
     print('Observation space:', env.observation_space)
     print('Action space:', env.action_space)
 
-    replay_buffer = ReplayBuffer(4096)
+    replay_buffer = ReplayBuffer(2048)
 
     representation, dynamics, prediction, action_sampler, variables = define_model(
         env)
-    loss_r, loss_v, loss_p, regularization = define_losses(variables)
+    loss_r, loss_v, loss_p, regularization = define_losses(env, variables)
     muzero = MuZeroPSO(representation, dynamics, prediction)
 
-    optimizer = tf.optimizers.Adam(0.005)
+    optimizer = tf.optimizers.Adam(0.003)
 
     attempt = 0
     while True:
@@ -132,8 +142,8 @@ def main():
         attempt += 1
 
         # Training phase
-        for _ in range(8):
-            batch = replay_buffer.sample(256, 9)
+        for _ in range(16):
+            batch = replay_buffer.sample(128, 5)
 
             obs, actions, rewards, obs_tp1, dones = zip(
                 *[zip(*entry) for entry in batch])
