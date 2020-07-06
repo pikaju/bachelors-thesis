4245ad4d288d911b86b48ee5e271c105379c27ed
Julien Scholz <julienscholzo@hotmail.de>
2020-07-06 21:26:13 +0200

    Simplify how replay buffer is sampled

--- implementation/src/replay_buffer.py
+++ implementation/src/replay_buffer.py
@@ -18,29 +18,11 @@ class PrioritizedReplayBuffer:
     def sample(self, batch_size, rollout_size):
         batch = []
 
-        def unroll(index):
-            unrolled = np.zeros([rollout_size], dtype=object)
-            data_index = index - self._tree.capacity + 1
-
-            for offset in range(rollout_size):
-                # The write head breaks experience chains.
-                if data_index == self._tree.write:
-                    return None
-                data = self._tree.data[data_index]
-                # Don't allow done = True in the middle of a rollout.
-                if offset < rollout_size - 1 and data[-1]:
-                    return None
-                unrolled[offset] = data
-                data_index = (data_index + 1) % self._tree.capacity
-            return unrolled
-
-        while len(batch) < batch_size:
+        for _ in range(batch_size):
             pointer = random.uniform(0, self._tree.total())
-            index, probability, _ = self._tree.get(pointer)
-            unrolled = unroll(index)
-            if unrolled is not None:
-                weight = ((1.0 / batch_size) *
-                          (1.0 / probability)) ** self.beta
-                batch.append((index, probability, weight, unrolled))
+            index, probability, data = self._tree.get(pointer)
+            weight = ((1.0 / batch_size) *
+                      (1.0 / probability)) ** self.beta
+            batch.append((index, probability, weight, data))
 
         return batch
