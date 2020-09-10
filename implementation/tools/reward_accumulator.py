a0e8110792466f24ea5714015914f9c864019519
Julien Scholz <julienscholzo@hotmail.de>
2020-09-10 16:08:21 +0200

    Add first results

--- implementation/tools/reward_accumulator.py
+++ implementation/tools/reward_accumulator.py
@@ -5,16 +5,15 @@ ax = plt.gca()
 
 for run in ['state_lr_0.0', 'state_lr_0.5', 'state_lr_1.0']:
     frame = pd.DataFrame()
-    for test in range(8):
-        url = 'http://localhost:6007/data/plugin/scalars/scalars?tag=reward%2Ftotal&run={}%2Ftest{}&format=csv'.format(
+    for test in range(29):
+        url = 'http://localhost:6006/data/plugin/scalars/scalars?tag=reward%2Ftotal&run={}%2Ftest{}&format=csv'.format(
             run, test)
         data = pd.read_csv(url)
         frame = pd.concat([frame, data])
-    frame = frame.groupby(frame.index).mean()
-    frame = frame.head(80).rename(columns={'Value': run})
-    idx = len(frame) - 1 if len(frame) % 2 else len(frame)
-    frame = frame[:idx].groupby(frame.index[:idx] // 1).mean()
-    print(frame)
+    frame = frame.groupby(frame.index).median()
+    frame = frame.head(150).rename(columns={'Value': run})
+    # idx = len(frame) - 1 if len(frame) % 2 else len(frame)
+    # frame = frame[:idx].groupby(frame.index[:idx] // 2).mean()
     frame.plot(x='Step', y=run, ax=ax)
 
 plt.show()
