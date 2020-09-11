1791ba889681d8e335cca155663c0994a0b1d27e
Julien Scholz <julienscholzo@hotmail.de>
2020-09-11 13:33:54 +0200

    Add paragraph about policy and value functions

--- implementation/tools/reward_accumulator.py
+++ implementation/tools/reward_accumulator.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 ax = plt.gca()
 
-for run in ['state_lr_0.0', 'state_lr_0.5', 'state_lr_1.0']:
+for run in ['generation_lr_0.0', 'generation_lr_0.5', 'generation_lr_1.0']:
     frame = pd.DataFrame()
-    for test in range(29):
+    for test in range(6):
         url = 'http://localhost:6006/data/plugin/scalars/scalars?tag=reward%2Ftotal&run={}%2Ftest{}&format=csv'.format(
             run, test)
         data = pd.read_csv(url)
         frame = pd.concat([frame, data])
-    frame = frame.groupby(frame.index).median()
-    frame = frame.head(150).rename(columns={'Value': run})
+    frame = frame.groupby(frame.index).mean()
+    frame = frame.head(256).rename(columns={'Value': run})
     # idx = len(frame) - 1 if len(frame) % 2 else len(frame)
     # frame = frame[:idx].groupby(frame.index[:idx] // 2).mean()
     frame.plot(x='Step', y=run, ax=ax)
