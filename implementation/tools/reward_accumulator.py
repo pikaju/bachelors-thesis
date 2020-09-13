e9b1dc4750d40e79a871dd6956fbfb3eb239a948
Julien Scholz <julienscholzo@hotmail.de>
2020-09-13 13:51:09 +0200

    Add actor-critic chapter

--- implementation/tools/reward_accumulator.py
+++ implementation/tools/reward_accumulator.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 ax = plt.gca()
 
-for run in ['generation_lr_0.0', 'generation_lr_0.5', 'generation_lr_1.0']:
+for run in ['state_lr_0.0', 'state_lr_0.5', 'state_lr_1.0']:
     frame = pd.DataFrame()
-    for test in range(0, 38):
+    for test in range(0, 15):
         url = 'http://localhost:6006/data/plugin/scalars/scalars?tag=reward%2Ftotal&run={}%2Ftest{}&format=csv'.format(
             run, test)
         print(url)
         data = pd.read_csv(url)
         frame = pd.concat([frame, data])
-    frame = frame.groupby(frame.index).mean()
+    frame = frame.groupby(frame.index).median()
     frame = frame.head(256).rename(columns={'Value': run})
     # idx = len(frame) - 1 if len(frame) % 2 else len(frame)
     # frame = frame[:idx].groupby(frame.index[:idx] // 2).mean()
