5ebaf4b152d117ab898a7c06aca14f5b81ac9bb9
Julien Scholz <julienscholzo@hotmail.de>
2020-09-18 16:34:35 +0200

    Adjust reward_accumulator.py

--- implementation/tools/reward_accumulator.py
+++ implementation/tools/reward_accumulator.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 
 ax = plt.gca()
 
-for run in ['state_lr_0.0', 'state_lr_0.5', 'state_lr_1.0']:
+for run in ['lr0.0', 'lr0.5', 'lr1.0']:
     frame = pd.DataFrame()
-    for test in range(0, 15):
-        url = 'http://localhost:6006/data/plugin/scalars/scalars?tag=reward%2Ftotal&run={}%2Ftest{}&format=csv'.format(
+    for test in range(0, 7):
+        url = 'http://0.0.0.0:6006/data/plugin/scalars/scalars?tag=1.Total+reward%2F1.Total+reward&run={}%2Frun{}&format=csv'.format(
             run, test)
         print(url)
         data = pd.read_csv(url)
         frame = pd.concat([frame, data])
     frame = frame.groupby(frame.index).median()
-    frame = frame.head(256).rename(columns={'Value': run})
-    # idx = len(frame) - 1 if len(frame) % 2 else len(frame)
-    # frame = frame[:idx].groupby(frame.index[:idx] // 2).mean()
+    frame = frame.head(18000).rename(columns={'Value': run})
+    idx = len(frame) - 1 if len(frame) % 32 else len(frame)
+    frame = frame[:idx].groupby(frame.index[:idx] // 32).mean()
     frame.plot(x='Step', y=run, ax=ax)
 
 plt.show()
