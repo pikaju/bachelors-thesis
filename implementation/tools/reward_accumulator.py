3428f82c4db8edbb96cf143ff2ae6c045332f6a9
Julien Scholz <julienscholzo@hotmail.de>
2020-09-19 22:47:40 +0200

    Finish up deep RL section

--- implementation/tools/reward_accumulator.py
+++ implementation/tools/reward_accumulator.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 
 ax = plt.gca()
 
-for run in ['lr0.0', 'lr0.5', 'lr1.0']:
+for run in ['lr0.0', 'lr1.0']:
     frame = pd.DataFrame()
-    for test in range(0, 7):
+    for test in range(0, 16):
         url = 'http://0.0.0.0:6006/data/plugin/scalars/scalars?tag=1.Total+reward%2F1.Total+reward&run={}%2Frun{}&format=csv'.format(
             run, test)
         print(url)
         data = pd.read_csv(url)
         frame = pd.concat([frame, data])
     frame = frame.groupby(frame.index).median()
-    frame = frame.head(18000).rename(columns={'Value': run})
-    idx = len(frame) - 1 if len(frame) % 32 else len(frame)
-    frame = frame[:idx].groupby(frame.index[:idx] // 32).mean()
+    frame = frame.rename(columns={'Value': run})
+    idx = len(frame) - 1 if len(frame) % 8 else len(frame)
+    frame = frame[:idx].groupby(frame.index[:idx] // 8).mean()
     frame.plot(x='Step', y=run, ax=ax)
 
 plt.show()
